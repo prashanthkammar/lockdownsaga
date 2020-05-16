9b20478f7b0f2e1e5b28689e4be03b5bff3b9bf0
Prashanth G K <prashkammar17@gmail.com>
2020-05-17 01:14:37 +0530

    white spaces

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ import requests
 app = Flask(__name__)
 app.secret_key = "eflwsrkmksj82398reyd0873eirds0923kjo83r7iwerwed"
 
+strip_filter = lambda x: x.strip() if x else None
+
 class RegisterForm(Form):
     Name = StringField('Name', validators= [InputRequired()], render_kw={"placeholder":"Name","id":"name","autocomplete":"off","onfocus":"this.removeAttribute('readonly');" })
-    Email = StringField('Email', validators = [InputRequired(), Email(message='Invalid email')], render_kw={"placeholder":"Email ID","id":"email","autocomplete":"off","onfocus":"this.removeAttribute('readonly');" })    
-    Phone = StringField('Phone', validators = [InputRequired(),Length(min = 10, max = 13, message = 'Enter your valid mobile number')], render_kw={"placeholder":"Contact No.","type":"number","id":"phone","autocomplete":"off", "onfocus":"this.removeAttribute('readonly');" })
+    Email = StringField('Email', validators = [InputRequired(), Email(message='Invalid email')], filters=[strip_filter], render_kw={"placeholder":"Email ID","id":"email","autocomplete":"off","onfocus":"this.removeAttribute('readonly');" })    
+    Phone = StringField('Phone', validators = [InputRequired(),Length(min = 10, max = 13, message = 'Enter your valid mobile number')], filters=[strip_filter], render_kw={"placeholder":"Contact No.","type":"number","id":"phone","autocomplete":"off", "onfocus":"this.removeAttribute('readonly');" })
     Password = PasswordField('Password', validators = [ InputRequired(), Length(min = 6, max = 30, message = 'Password length must be atleast 6'), EqualTo('Confirm', message = "Passwords must match")], render_kw={"placeholder":"Password","id":"pass","autocomplete":"off","onfocus":"this.removeAttribute('readonly');" })
     Confirm = PasswordField('Confirm Password', render_kw={"placeholder":"Confirm Password","id":"confirm","autocomplete":"off", "onfocus":"this.removeAttribute('readonly');" })
 
@@ -43,7 +45,6 @@ def register():
         session=requests.Session()
         session.post('https://ucsp-quiz.herokuapp.com/register',data=payload)
         session.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDpKtHKmbF-Ctvyno1OtpoJJOktnrCx8g4',data=data)
-
         flash(" Dear {}, your registration was successful!".format(form.Name.data),"message") 
         return redirect(url_for('inst'))
 
